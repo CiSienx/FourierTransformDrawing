ac88749c11589da9af4d195162218be5a24d63f3
CiSienx <78628913+CiSienx@users.noreply.github.com>
2021-04-02 09:14:06 +0530

    Create RecordCoordinates.py

--- RecordCoordinates.py
+++ RecordCoordinates.py
@@ -6,16 +6,16 @@ pg.init()
 screen = pg.display.set_mode(size)
 switch = True
 
-image = pg.image.load("sample.jpg")
-image = pg.transform.scale(image,(500,500))
-screen.blit(image,(0,0))
 
 new_coords = []
 
 while switch:
     for event in pg.event.get():
         coord = (0,0)
-        if pg.mouse.get_pressed()[0] :
+        if event.type == pg.QUIT:
+            pg.quit()
+            break
+        elif pg.mouse.get_pressed()[0] :
             coord = pg.mouse.get_pos()
             new_coords.append(coord)
             pg.draw.circle(screen,(255,255,255),coord,1)
@@ -32,7 +32,7 @@ while switch:
                 for i in range(len(new_coords)):
                     pg.draw.circle(screen, (255, 255, 255), new_coords[i], 1)
         elif event.type == pg.KEYDOWN and pg.key.get_pressed()[pg.K_s]:
-            file = open("coordinate_love.txt", "w")
+            file = open("coordinate.txt", "w")
             json.dump(new_coords,file)
             print(len(new_coords))
             file.close()
